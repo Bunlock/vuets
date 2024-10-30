86ff108816c20f81a5db9dbd78bdacf17c878f9b
Jules Lepoivre <jlepoivre@saki-studio.com>
2024-10-30 15:20:55 +0100

    init

---
+++ src/components/OrderList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import type { Order } from '../interfaces/order'
+
+function addOrder() {
+  const customer = window.prompt('customer')!
+  const date = window.prompt('date')!
+  const amount = Number(window.prompt('amount'))!
+  const id = crypto.randomUUID()
+  console.log(id)
+  orders.push({ id, customer, date, amount })
+}
+
+const orders = reactive<Order[]>([])
+</script>
+
+<template>
+  <button @click="addOrder()">add Order</button>
+
+  <h1>Orders</h1>
+  <div v-for="order in orders" :key="order.id">
+    {{ order }}
+  </div>
+</template>
---
+++ src/interfaces/order.ts
@@ -0,0 +1,6 @@
+export interface Order {
+  id: string
+  customer: string
+  date: string
+  amount: number
+}
